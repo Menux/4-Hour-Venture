671dc87850939d983521f7c6cc78ced3c9a339e8
Marcel Engelmann <engelmann.marcel@yahoo.de>
2017-02-23 12:16:32 +0100

    Add countdown, meta tags and minor bug fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,41 @@
 // JavaScript Codes
-$(function(){
+$(function() {
 
-  //jQuery to collapse the navbar on scroll
-  $(window).scroll(function() {
-      if ($(".navbar").offset().top > 50) {
-          $(".navbar-fixed-top").addClass("top-nav-collapse");
-      } else {
-          $(".navbar-fixed-top").removeClass("top-nav-collapse");
-      }
-  });
+    // ## Scrolling
+    //jQuery to collapse the navbar on scroll
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
 
-  //jQuery for page scrolling feature - requires jQuery Easing plugin
-  $(function() {
-      $('a.page-scroll').bind('click', function(event) {
-          var $anchor = $(this);
-          $('html, body').stop().animate({
-              scrollTop: $($anchor.attr('href')).offset().top
-          }, 1500, 'easeInOutExpo');
-          event.preventDefault();
-      });
-  });
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    // ## Count Down
+    $("#countdown").countdown("2017/04/05 17:00:00", {elapse: true}).on('update.countdown', function(event) {
+        if (event.elapsed && event.strftime('%H') === "06") {
+            $(this).text(event.strftime('Das Event ist schon zu Ende. Bis zum nächsten Mal.'));
+        } else if (event.elapsed) {
+            $(this).text(event.strftime('Das Event läuft grade auf hochtouren.'));
+        } else {
+            $(this).text(event.strftime('Noch %D Tage und %H:%M:%S bis zum 1.Four Hour Venture.'));
+        }
+    });
+
 });
 
+// ## Google Maps
 function initialize() {
     var latlng = new google.maps.LatLng(52.1791125, 10.5587162);
     var myOptions = {
@@ -34,8 +48,7 @@ function initialize() {
         draggable: false,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map"),
-myOptions);
+    var map = new google.maps.Map(document.getElementById("map"), myOptions);
 }
 var myKey = "AIzaSyCSpMm9sAWUDHV-RIgK1mDsRGMDFPLjKRQ";
 function loadScript() {
