a93b27dc644d3ba23bdbf1039c49c7878b3a3bf9
Marcel Engelmann <engelmann.marcel@yahoo.de>
2017-02-23 14:34:06 +0100

    Fix Navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,25 @@
-// JavaScript Codes
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-
-    // ## Scrolling
-    //jQuery to collapse the navbar on scroll
-    $(window).scroll(function() {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar-fixed-top").addClass("top-nav-collapse");
-        } else {
-            $(".navbar-fixed-top").removeClass("top-nav-collapse");
-        }
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
     });
+});
 
-    //jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(function() {
-        $('a.page-scroll').bind('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top
-            }, 1500, 'easeInOutExpo');
-            event.preventDefault();
-        });
-    });
-
-    // ## Count Down
+// ## Count Down
+$(function() {
     $("#countdown").countdown("2017/04/05 17:00:00", {elapse: true}).on('update.countdown', function(event) {
         if (event.elapsed && event.strftime('%H') === "06") {
             $(this).text(event.strftime('Das Event ist schon zu Ende. Bis zum nächsten Mal.'));
