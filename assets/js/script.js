4e2012380500b87d0a8a6f9ca544a124f6da0d47
Marcel Engelmann <mail@marcelengelmann.de>
2017-04-11 13:25:51 +0200

    Update - Next event

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ $(function() {
 
 // ## Count Down
 $(function() {
-    $("#countdown").countdown("2017/04/04 17:00:00", {elapse: true}).on('update.countdown', function(event) {
+    $("#countdown").countdown("2017/10/10 17:00:00", {elapse: true}).on('update.countdown', function(event) {
         if (event.elapsed && event.strftime('%H') === "06") {
             $(this).text(event.strftime('Das Event ist schon zu Ende. Bis zum nächsten Mal.'));
         } else if (event.elapsed) {
